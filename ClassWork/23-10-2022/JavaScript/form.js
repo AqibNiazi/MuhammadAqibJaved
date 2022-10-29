0b7b81dedf516e2b977d78d6e0ebd1e1dc4e694e
Muhammad Aqib Javed <aqibjaved5201@gmail.com>
2022-10-29 22:39:26 +0500

    form Updated

--- ClassWork/23-10-2022/JavaScript/form.js
+++ ClassWork/23-10-2022/JavaScript/form.js
@@ -1,16 +1,33 @@
 
 
-let x=()=>{
-let fname=document.getElementById('fname').value;
 
-let lname=document.getElementById('lname').value;
 
-let email=document.getElementById('email').value;
-let program=document.querySelector('#Program').value;
+function checkBox() {
+    var yes = document.getElementById("yes");
+    var no = document.getElementById("no");
 
-let demo=document.getElementById('demo');
-
-let genderValue = document.querySelector('input[name="gender"]:checked').value;
-demo.innerHTML=`first Name : ${fname} </br></br> Last Name : ${lname} </br></br> Email : ${email} </br></br> Program : ${program}  </br></br> Gender : ${genderValue}`;
-
+    if (yes.checked == true) {
+        let y = document.getElementById("yes").value;
+        return document.getElementById("demo").innerHTML = y;
+    }
+    else if (no.checked == true) {
+        var n = document.getElementById("no").value;
+        return document.getElementById("demo").innerHTML = n;
+    }
+    else {
+        return document.getElementById("demo").innerHTML = "*Please mark any of checkbox";
+    }
 }
+
+
+let printRecord = () => {
+    let fname = document.getElementById('fname').value;
+    let lname = document.getElementById('lname').value;
+    let email = document.getElementById('email').value;
+    let program = document.querySelector('#Program').value;
+    let demo = document.getElementById('demo');
+    let genderValue = document.querySelector('input[name="gender"]:checked').value;
+
+    return demo.innerHTML = `first Name : ${fname} </br></br> Last Name : ${lname} </br></br> Email : ${email} </br></br> Program : ${program}  </br></br> Gender : ${genderValue} </br></br> Job : ${checkBox()}`;
+}
+
