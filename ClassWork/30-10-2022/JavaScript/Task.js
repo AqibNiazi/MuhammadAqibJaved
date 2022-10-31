ef7c8f32e4106791687d8b36b91a134c42b3be53
Muhammad Aqib Javed <aqibjaved5201@gmail.com>
2022-10-31 20:04:04 +0500

    Task 1-6 completed

--- ClassWork/30-10-2022/JavaScript/Task.js
+++ ClassWork/30-10-2022/JavaScript/Task.js
@@ -1,16 +1,17 @@
-const addition = (a, b) =>  a + b;
-console.log(`Addition ${addition(3, 4)}`);
+// const addition = (a, b) =>  a + b;
+// console.log(`Addition ${addition(3, 4)}`);
 
-const subtraction = (a, b) => a - b;
-console.log(`subtraction :${subtraction(3, 4)}`);
+// const subtraction = (a, b) => a - b;
+// console.log(`subtraction :${subtraction(3, 4)}`);
 
-const square=(num)=> num*2;
-console.log(`Square : ${square(2)}`);
+// const square=(num)=> num*2;
+// console.log(`Square : ${square(2)}`);
 
-const divide=(num)=> num / 2;
-console.log(`Divide :${divide(4)}`);
+// const divide=(num)=> num / 2;
+// console.log(`Divide :${divide(4)}`);
 
-//                          Temparature Converter
+// //                            Temparature Converter
+
 const converter=(temp,char)=>{
 if(char==='C'){
    let fahrenheit= (parseFloat(temp*1.8)+32).toFixed(2)+"F";       //Celsius to Fahrenheit
@@ -24,62 +25,128 @@ if(char==='C'){
 }
 console.log(converter(20,'C'));
 
-const obj = {
-  name: "Muhammad Aqib javed",
-  university: "UET Taxila",
-  calculatePercentage: (num) => {
-    return `${((num / 1100) * 100).toFixed(2)} %`;
-  },
-};
-console.log(
-  `Name : ${obj.name}\n University : ${
-    obj.university
-  }\n Percentage :${obj.calculatePercentage(992)}`
-);
+// const obj = {
+//   name: "Muhammad Aqib javed",
+//   university: "UET Taxila",
+//   calculatePercentage: (num) => {
+//     return `${((num / 1100) * 100).toFixed(2)} %`;
+//   },
+// };
+// console.log(
+//   `Name : ${obj.name}\n University : ${
+//     obj.university
+//   }\n Percentage :${obj.calculatePercentage(992)}`
+// );
 
-//   
+// //   
 
-// //                                Task 1
-const repeatElements = (elem, n)=>{
-    const arr = [];
+// // //                                Task 1
+// const repeatElements = (elem, n)=>{
+//     const arr = [];
 
-    for (let i = 0; i < n; i++) {
-        arr.push(elem);
-    };
+//     for (let i = 0; i < n; i++) {
+//         arr.push(elem);
+//     };
 
-    return arr;
-};
-console.log(repeatElements('a',3));
+//     return arr;
+// };
+// console.log(repeatElements('a',3));
 
-// //                Task 2
-const revertArray=(input)=>{
-    var newArr=[];
-    for(let i=input.length-1;i>=0;i--){
-        newArr.push(input[i]);
-    }
-    return newArr;
-}
-let arr=[1,2,3];
-console.log(arr);
-console.log(revertArray(arr));
-//                                      Task 2 with forEach Loop
-const reverseArray=(arr)=>{
-const array=[];
-  arr.forEach(element => {
-    array.unshift(element);
-  });
-  return array;
-}
-console.log(reverseArray([1,2,3,4,5]));
+// // //                Task 2
+// const revertArray=(input)=>{
+//     var newArr=[];
+//     for(let i=input.length-1;i>=0;i--){
+//         newArr.push(input[i]);
+//     }
+//     return newArr;
+// }
+// let arr=[1,2,3];
+// console.log(arr);
+// console.log(revertArray(arr));
+// //                                      Task 2 with forEach Loop
+// const reverseArray=(arr)=>{
+// const array=[];
+//   arr.forEach(element => {
+//     array.unshift(element);
+//   });
+//   return array;
+// }
+// console.log(reverseArray([1,2,3,4,5]));
 
-//                                              Task 3
-const removeElement=(arr)=>{
-return arr.filter((item)=>{
-return Boolean(item);
-})
-}
-const array=[0,1,false,2,undefined,2,3,'',null]
-console.log(removeElement(array));
+// //  //                                            Task 3
+// const removeElement=(arr)=>{
+// return arr.filter((item)=>{
+// return Boolean(item);
+// })
+// }
+// const array=[0,1,false,2,undefined,2,3,'',null]
+// console.log(removeElement(array));
+
+//  //                                            Task 4
+//  //   write a method that return duplicate free array
+
+//   //                                        using set method
+// let array=[1,2,3,1,2];
+
+// const removeDuplicates=(array)=>{
+//   return [...new Set(array)]
+// }
+// console.log(removeDuplicates(array));
+
+
+//   //                                           using filter method
+// const getDuplicate=(data)=>{
+// return data.filter((item,index)=>data.indexOf(item)===index);
+// }
+// console.log(getDuplicate(array));
+
+
+//   //                                        Task 5
+//Multiply each array element and return a new array
+
+
+// // Using for loop multiply each array element and return new array
+
+
+// let array=[1,2,3];
+// const multipleArray=(array,num)=>{
+
+// let newArray=[];
+//   for(let i=0;i<array.length;i++){
+//     newArray.push(array[i]*num);
+//   }
+//   return newArray;
+// }
+// console.log(multipleArray(array,4));
+
+// //                                    using Map method 
+// let arr = [1, 2, 3, 4];
+// const multipleArray=(array,num)=>{
+//   let modifiedArray = array.map((element)=>{
+//     return element *num;
+// })
+// return modifiedArray;
+// }
+// console.log(multipleArray(arr,4));
+
+
+
+//  //                                 Task 6
+// //   Replace an element at specified indexed
+//  //  array=[1,2,4,5]    ,index=3, newvalue=10
+
+
+// const replaceArrayElment=(array1)=>{
+//   array1.splice(3,1,10);
+// return array1;
+// }
+// let array=[1,2,4,5];
+// console.log(replaceArrayElment(array));
+
+
+
+
+
 
 // const obj={a:10};
 // obj.a=15;
@@ -89,14 +156,14 @@ console.log(removeElement(array));
 // console.log(obj);
 
 
-const person = {
-    address :  {
-       city:"Taxila",
-       country:"pakistan"
-    },
-    getAddress(){
-return `City :${this.address.city}\nCountry :${this.address.country}` 
-    }
-  };
-  console.log(person.getAddress());
+// const person = {
+//     address :  {
+//        city:"Taxila",
+//        country:"pakistan"
+//     },
+//     getAddress(){
+// return `City :${this.address.city}\nCountry :${this.address.country}` 
+//     }
+//   };
+//   console.log(person.getAddress());
   
--- ClassWork/30-10-2022/JavaScript/string.js
+++ ClassWork/30-10-2022/JavaScript/string.js
@@ -1,25 +1,25 @@
-//                                  string                                          //
+//                                    string                                          //
 
 //                                   Concat()
 
-// let fname="Aqib";
-// let lname=" Javed";
-// console.log(`Full Name: ${fname.concat(lname)}`);
+let fname="Aqib";
+let lname=" Javed";
+console.log(`Full Name: ${fname.concat(lname)}`);
 
 // //                                  IndexOf()                                   //
 
-// let str="My city is Talagang.";
-// console.log(str.indexOf("Talagang"));
+let str1="My city is Talagang.";
+console.log(str1.indexOf("Talagang"));
 
 // //                                  lastIndexOf()                                   //
 
-// let str1="My city is Talagang. Talagang is new District.";
-// console.log(str1.lastIndexOf("Talagang"));
+let str2="My city is Talagang. Talagang is new District.";
+console.log(str2.lastIndexOf("Talagang"));
 
 // //                                  charAt()                                   //
 
-// let str2="My city is Talagang. Talagang is new District.";
-// console.log(str2.charAt("i"));
+let str3="My city is Talagang. Talagang is new District.";
+console.log(str3.charAt("i"));
 
 //                              Naming Convention
 // camel Case    : fullName
@@ -28,12 +28,12 @@
 
 //                                        other useful methods                               //
 
-// let city="Talagang";
-// console.log(city.toUpperCase());
-// console.log(city.toLowerCase());
-// console.log(city.replace('T','t'));
+let city="Talagang";
+console.log(city.toUpperCase());
+console.log(city.toLowerCase());
+console.log(city.replace('T','t'));
 
-// let str = "aqibjaved5201@gmail.com";
-// let splittedString = str.split(".");
-// console.log(splittedString[1]);
+let str = "aqibjaved5201@gmail.com";
+let splittedString = str.split(".");
+console.log(splittedString[1]);
 
