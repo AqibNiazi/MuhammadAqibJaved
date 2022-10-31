cc42afdce2848417f9428ead9c7c4c134c28ef61
Muhammad Aqib Javed <aqibjaved5201@gmail.com>
2022-10-31 20:22:55 +0500

    Task 1-6 completed

--- ClassWork/30-10-2022/JavaScript/Task.js
+++ ClassWork/30-10-2022/JavaScript/Task.js
@@ -1,14 +1,14 @@
-// const addition = (a, b) =>  a + b;
-// console.log(`Addition ${addition(3, 4)}`);
+const addition = (a, b) =>  a + b;
+console.log(`Addition ${addition(3, 4)}`);
 
-// const subtraction = (a, b) => a - b;
-// console.log(`subtraction :${subtraction(3, 4)}`);
+const subtraction = (a, b) => a - b;
+console.log(`subtraction :${subtraction(3, 4)}`);
 
-// const square=(num)=> num*2;
-// console.log(`Square : ${square(2)}`);
+const square=(num)=> num*2;
+console.log(`Square : ${square(2)}`);
 
-// const divide=(num)=> num / 2;
-// console.log(`Divide :${divide(4)}`);
+const divide=(num)=> num / 2;
+console.log(`Divide :${divide(4)}`);
 
 // //                            Temparature Converter
 
@@ -24,81 +24,77 @@ if(char==='C'){
 }
 }
 console.log(converter(20,'C'));
+//                                calculate percentage of numbers 
 
-// const obj = {
-//   name: "Muhammad Aqib javed",
-//   university: "UET Taxila",
-//   calculatePercentage: (num) => {
-//     return `${((num / 1100) * 100).toFixed(2)} %`;
-//   },
-// };
-// console.log(
-//   `Name : ${obj.name}\n University : ${
-//     obj.university
-//   }\n Percentage :${obj.calculatePercentage(992)}`
-// );
+const obj = {
+  name: "Muhammad Aqib javed",
+  university: "UET Taxila",
+  calculatePercentage: (num) => {
+    return `${((num / 1100) * 100).toFixed(2)} %`;
+  },
+};
+console.log(
+  `Name : ${obj.name}\n University : ${
+    obj.university
+  }\n Percentage :${obj.calculatePercentage(992)}`
+);
 
 // //   
 
 // // //                                Task 1
-// const repeatElements = (elem, n)=>{
-//     const arr = [];
+const repeatElements = (elem, n)=>{
+    const arr = [];
 
-//     for (let i = 0; i < n; i++) {
-//         arr.push(elem);
-//     };
+    for (let i = 0; i < n; i++) {
+        arr.push(elem);
+    };
 
-//     return arr;
-// };
-// console.log(repeatElements('a',3));
+    return arr;
+};
+console.log(repeatElements('a',3));
 
 // // //                Task 2
-// const revertArray=(input)=>{
-//     var newArr=[];
-//     for(let i=input.length-1;i>=0;i--){
-//         newArr.push(input[i]);
-//     }
-//     return newArr;
-// }
-// let arr=[1,2,3];
-// console.log(arr);
-// console.log(revertArray(arr));
+const revertArray=(input)=>{
+    var newArr=[];
+    for(let i=input.length-1;i>=0;i--){
+        newArr.push(input[i]);
+    }
+    return newArr;
+}
+console.log(revertArray([1,2,3]));
 // //                                      Task 2 with forEach Loop
-// const reverseArray=(arr)=>{
-// const array=[];
-//   arr.forEach(element => {
-//     array.unshift(element);
-//   });
-//   return array;
-// }
-// console.log(reverseArray([1,2,3,4,5]));
+const reverseArray=(arr)=>{
+const array=[];
+  arr.forEach(element => {
+    array.unshift(element);
+  });
+  return array;
+}
+console.log(reverseArray([1,2,3,4,5]));
 
 // //  //                                            Task 3
-// const removeElement=(arr)=>{
-// return arr.filter((item)=>{
-// return Boolean(item);
-// })
-// }
-// const array=[0,1,false,2,undefined,2,3,'',null]
-// console.log(removeElement(array));
+const removeElement=(arr)=>{
+return arr.filter((item)=>{
+return Boolean(item);
+})
+}
+console.log(removeElement([0,1,false,2,undefined,2,3,'',null]));
 
 //  //                                            Task 4
 //  //   write a method that return duplicate free array
 
 //   //                                        using set method
-// let array=[1,2,3,1,2];
-
-// const removeDuplicates=(array)=>{
-//   return [...new Set(array)]
-// }
-// console.log(removeDuplicates(array));
+const removeDuplicates=(array)=>{
+  return [...new Set(array)]
+}
+console.log(removeDuplicates([1,2,3,1,2]));
 
 
 //   //                                           using filter method
-// const getDuplicate=(data)=>{
-// return data.filter((item,index)=>data.indexOf(item)===index);
-// }
-// console.log(getDuplicate(array));
+const getDuplicate=(data)=>{
+return data.filter((item,index)=>data.indexOf(item)===index);
+}
+console.log(getDuplicate([1,2,3,1,2]));
 
 
 //   //                                        Task 5
@@ -108,26 +104,26 @@ console.log(converter(20,'C'));
 // // Using for loop multiply each array element and return new array
 
 
-// let array=[1,2,3];
-// const multipleArray=(array,num)=>{
+let array=[1,2,3];
+const multiplyArray=(array,num)=>{
 
-// let newArray=[];
-//   for(let i=0;i<array.length;i++){
-//     newArray.push(array[i]*num);
-//   }
-//   return newArray;
-// }
-// console.log(multipleArray(array,4));
+let newArray=[];
+  for(let i=0;i<array.length;i++){
+    newArray.push(array[i]*num);
+  }
+  return newArray;
+}
+console.log(multiplyArray(array,4));
 
 // //                                    using Map method 
-// let arr = [1, 2, 3, 4];
-// const multipleArray=(array,num)=>{
-//   let modifiedArray = array.map((element)=>{
-//     return element *num;
-// })
-// return modifiedArray;
-// }
-// console.log(multipleArray(arr,4));
+let arr = [1, 2, 3, 4];
+const multiplyArray1=(array,num)=>{
+  let modifiedArray = array.map((element)=>{
+    return element *num;
+})
+return modifiedArray;
+}
+console.log(multiplyArray(arr,4));
 
 
 
@@ -136,12 +132,12 @@ console.log(converter(20,'C'));
 //  //  array=[1,2,4,5]    ,index=3, newvalue=10
 
 
-// const replaceArrayElment=(array1)=>{
-//   array1.splice(3,1,10);
-// return array1;
-// }
-// let array=[1,2,4,5];
-// console.log(replaceArrayElment(array));
+const replaceArrayElment=(array)=>{
+  array.splice(3,1,10);
+return array;
+}
+let array1=[1,2,4,5];
+console.log(replaceArrayElment(array1));
 
 
 
